021da79f7b8c26ead285c5bdc65fbd8d001372b7
KARTAVYA PANTHI <97825212+Kartv07@users.noreply.github.com>
2023-06-07 13:29:39 +0530

    Update tailwind.config.cjs

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,13 +1,4 @@
-// /** @type {import('tailwindcss').Config} */ 
-// module.exports = {
-//   content: [
-//     "./src/**/*.{js,jsx,ts,tsx}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
+
 
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
