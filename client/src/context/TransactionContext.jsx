0a94924da173a303c2ff923346114894739a155a
KARTAVYA PANTHI <97825212+Kartv07@users.noreply.github.com>
2023-06-07 13:33:33 +0530

    Update TransactionContext.jsx

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -7,7 +7,6 @@ export const TransactionContext = React.createContext();
 
 const { ethereum } = window;
 
-
 const getEthereumContract = () => {
     const provider = new ethers.providers.Web3Provider(ethereum);
     const signer = provider.getSigner();
